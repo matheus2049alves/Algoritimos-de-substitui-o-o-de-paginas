5af2dc68ab7d4bd8ff10512304ba9ae19bd412c1
diogo <diogobrasildasilva@gmail.com>
2024-06-03 00:30:44 -0300

    Adição das funções para criar taxa de frames.

--- algoritmos.py
+++ algoritmos.py
@@ -16,6 +16,7 @@ def fifo (num_frames, paginas, num_referencias):
     global taxa_falha
     global coluna
     
+    
 
     inicializar()
     registrar_falhas = []
@@ -37,7 +38,7 @@ def fifo (num_frames, paginas, num_referencias):
             if (len(s) < ii):
                 if (paginas[i] not in s):
                     s.add(paginas[i])
-                    page_faults += 1
+                    falha_pagina += 1
                     falha.append(True)
                     indexes.append(paginas[i])
                 else:
@@ -65,7 +66,7 @@ def fifo (num_frames, paginas, num_referencias):
 def nova_janela(capacity):
     global root
     root = Tk()
-    Basic_design(capacity)
+    configura_tabela(capacity)
     root.geometry("1600x660")
 
 #Função espaço vazio - Leo
@@ -77,6 +78,15 @@ def espaco_vazio():
     L = Label(root, text=" ", height="1", width="1")
     L.grid(row=linha, column=coluna)
     linha += 1
+
+
+# Se a referencia também estiver grande quando remover está função para melhor visuabilidade
+def construir_label_vazia():
+    global coluna
+    global linha
+    minha_label1= Label(root,text=" ",padx=15,pady=10)
+    minha_label1.grid(row=1,column=coluna+1)
+    coluna+=1
 
 #Função para quadradinhos - Leo
 
@@ -107,13 +117,55 @@ def configura_tabela(N):
     falha_Label.grid(row=N+1,column=0,padx=20,pady=10)
 
 
-def Visualisar(tipo, num_frames, referencias):
+def Visualizar(tipo, num_frames, referencias):
     nFrames = (int)(num_frames)
     paginas = list(map(int, referencias.split(" ")))
     num_referencias = len(referencias)
 
     if tipo == "FIFO":
         fifo(nFrames, paginas, num_referencias)
+
+
+# Label para as falhas e taxas de acertos
+def taxa_frame(taxa_falha, Frames, frames, registro_falha, registro_acerto):
+    tam_coluna = int(Frames / 2)
+    frame1 = LabelFrame(root, text=" "+""+" Page Fault Ratio ",pady=15, padx=10)
+    frame1.configure(font=("Century Gothic", 15))
+    frame1.grid(row=Frames + 4, column=tam_coluna, columnspan=int(Frames))
+    taxa_acerto = 1 - taxa_falha
+    minhaLabel4 = Label(frame1, text=" Hit Ratio:   =", fg="green", bd=1, padx=10, pady=15, relief=FLAT)
+    minhaLabel4.configure(font=("Century Gothic", 14, 'bold'))
+    minhaLabel4.grid(row=1, column=0)
+    minhaLabel5 = Label(frame1, text="Fault Ratio: =", fg="red", bd=1, padx=10, pady=15, relief=FLAT)
+    minhaLabel5.configure(font=("Century Gothic", 14, 'bold'))
+    minhaLabel5.grid(row=2, column=0)
+    e2 = Label(frame1, text=str(round(taxa_acerto,11)), borderwidth=3)
+    e2.configure(font=("Century Gothic", 14))
+    e2.grid(row=1, column=1)
+    e3 = Label(frame1, text=str(round(taxa_falha,11)), borderwidth=3)
+    e3.configure(font=("Century Gothic", 14))
+    e3.grid(row=2, column=1)
+    Graph = Button(root, borderwidth="0", text="Show Graph", bg="#e8e8e8", fg="green", font=("Century Gothic", 15),
+               activeforeground="black", activebackground="#bbbfca", command=lambda: graphy(frames, registro_falha, registro_acerto))
+    Graph.grid(row=Frames+5, column=tam_coluna, columnspan=int(Frames), pady=18)
+    Back = Button(root, borderwidth="0", text="Back", bg="#e8e8e8", fg="green", font=("Century Gothic", 15),
+               activeforeground="black", activebackground="#bbbfca", command=root.destroy)
+    Back.grid(row=Frames+6, column=tam_coluna, columnspan=int(Frames))
+
+
+
+# Gráfico de acertos e flahas
+def graphy(frames, registrar_falhas, registrar_acertos):
+    fig = plt.figure(figsize=(8, 8), dpi=80)
+    plt.subplot(2, 1, 1)
+    plt.xlabel("No. Of Frames --->")
+    plt.ylabel("Fault --->")
+    plt.plot(frames, registrar_falhas, marker='x', color="red", ls="--")
+    plt.subplot(2, 1, 2)
+    plt.xlabel("No. Of Frames --->")
+    plt.ylabel("Hit --->")
+    plt.plot(frames, registrar_acertos, marker='o', color="green")
+    plt.show()
 
 def animar(num_referencias, paginas, imaginario, falha, taxa_falha, num_frames, frames, registrar_falhas, sem_falhas):
     global root
@@ -132,10 +184,10 @@ def animar(num_referencias, paginas, imaginario, falha, taxa_falha, num_frames, 
     for i in reversed(ls):
         quadro(i)
 
-    build_EmptyLabel()
+    construir_label_vazia()
 
     if (falha == True):
-        falha = "Falha"
+        teste_falha = "Falha"
         L1 = Label(root, text=teste_falha, fg="red")
         L1.configure(font=("Century Gothic", 12, 'bold'))
         L1.grid(row=linha, column=coluna - 1)
@@ -144,13 +196,28 @@ def animar(num_referencias, paginas, imaginario, falha, taxa_falha, num_frames, 
         teste_falha = "Sucesso"
         L1 = Label(root, text=teste_falha, font="Questrial", fg="green")
         L1.configure(font=("Century Gothic", 12, 'bold'))
-        L1.grid(row=row, column=col - 1)
+        L1.grid(row=row, column=coluna - 1)
         row += 1
-    FrameRatio(taxa_falha, num_frames, frames, registrar_falhas, sem_falhas)
+    taxa_frame(taxa_falha, num_frames, frames, registrar_falhas, sem_falhas)
 
+#-----------------------------------------------------------------------------------------------------------------------
+# Função para criar gráfico
+def grafico(numero_frames, referencias):
+    plotar_lista=[]
+    algoritmos=["FIFO"]
+    imaginario=0
+    num = int(numero_frames)
+    pagina_referencia = list(referencias.split(" "))
+    paginas = len(pagina_referencia)
 
+    inicializar()
+    fifo(pagina_referencia, paginas, num)
+    imaginario=taxa_falha
+    plotar_lista.append(imaginario)
 
-
+    fig = plt.figure()
+    plt.bar(algoritmos, plotar_lista)
+    plt.show()
 
 
 # Janela principal onde ficam as entradas de número de páginas e referências de páginas
@@ -184,6 +251,6 @@ label3.pack()
 referências = Entry(frame1, width="20", border=0, justify= "center")
 referências.pack()
 
-visualizar_botao = Button(frame1, text="visualizar", width="20",command=lambda : Visualisar(tipo_algoritmo.get(),num_frames.get(),referências.get() ) )
+visualizar_botao = Button(frame1, text="visualizar", width="20",command=lambda : Visualizar(tipo_algoritmo.get(),num_frames.get(),referências.get() ) )
 visualizar_botao.pack(pady="25")
 Menu.mainloop()
